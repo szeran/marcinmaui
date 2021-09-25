524e615782362366d312bb47a108d9e8f977625e
SZERYFWIN10F1\mm <marcin.malczewski@szeran.pl>
2021-09-25 20:17:46 +0200

    pierwsze próby odpalenia czegokolwiek

---
+++ B2B/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace B2B
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
